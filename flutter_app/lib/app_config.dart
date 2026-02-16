90935d461f8689a03ce883de194d8636fc417088
Qiiqii2i2i wiiwiw <49497668-hidar62@users.noreply.replit.com>
2026-02-16 02:08:39 +0000

    Flutter: set default APP_URL to hosted Replit app

--- flutter_app/lib/app_config.dart
+++ flutter_app/lib/app_config.dart
@@ -2,6 +2,5 @@ class AppConfig {
   // Set the target web app URL via build:
   // `flutter build apk --dart-define=APP_URL=https://...`
   static const String appUrl =
-      String.fromEnvironment('APP_URL', defaultValue: 'https://example.com/');
+      String.fromEnvironment('APP_URL', defaultValue: 'https://bc26eb03-4b04-4690-b4c7-fba18b5b1c36-00-1d71ogh8n44b9.sisko.replit.dev/');
 }
-
